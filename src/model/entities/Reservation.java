e4521bdefd1179ac62c6e000879746f0a9405c4d
Vinicius <gasparellovinicius@gmail.com>
2020-07-30 18:12:37 -0300

    bad solution

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,105 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+ 
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+    
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    
+    
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+   public Date getCheckOut() {
+        return checkOut;
+    }
+
+   public long duration(){
+       long deff = checkOut.getTime() - checkIn.getTime();
+       return TimeUnit.DAYS.convert(deff, TimeUnit.MILLISECONDS);
+   }
+    
+   public String updateDate(Date checkIn, Date checkOut){
+       
+	   Date now = new Date();
+       if (checkIn.before(now) || checkOut.before(now)) {
+       	return "Reservation dates for update must be future dates ";
+       	
+       }if(checkOut.after(checkIn)) {
+       	return "Check-out date must be after check-in date ";
+       }
+	   this.checkIn = checkIn;
+       this.checkOut = checkOut;
+       
+       return null;
+   }
+    
+   @Override
+   public String toString(){
+       return "Room"
+             + roomNumber
+             + ", check In: "
+             + sdf.format(checkIn)
+             + ", check Out: "
+             + sdf.format(checkOut)  
+             + ", "
+             + duration()
+             + " nights";
+   }
+   
+   
+   
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Reservation reservation = new Reservation(number, checkIn, checkOut);
+        	System.out.println("Reservation " + reservation);
+        
+			
+*/
